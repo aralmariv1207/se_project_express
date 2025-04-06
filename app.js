a27e26e790dfaff53089ea1df66cbed3333114b7
aralmariv1207 <aralmariv1207@users.noreply.github.com>
2025-04-06 13:04:45 -0400

    Update: 'app.js' and 'routes/users.js' files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use("/users", userRouter);
 
 const { PORT = 3001 } = process.env;
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-router.get("/", () => console.log("GET users"));
-router.get("/:userId", () => console.log("GET users by ID"));
-router.post("/", () => console.log("POST users"));
+router.get("/", (req, res) => {
+  res.json({ message: "GET users route" });
+});
+
+router.get("/:userId", (req, res) => {
+  res.json({ message: "GET users by ID"});
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: "POST users"});
+});
 
 module.exports = router;
